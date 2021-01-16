650489687e996caa28cd1ab4a63453a478874ab3
Johnny-18 <60463530+Johnny-18@users.noreply.github.com>
2021-01-16 15:16:21 +0200

    adding created project

---
+++ Project_1/Recipe_App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Recipe_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
